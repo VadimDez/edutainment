25d58fea7b0c9ae4c7e4e38c422e4041d6aa4f32
Vadym Yatsyuk <vasp3d@gmail.com>
2016-03-19 17:18:36 +0100

    some

--- js/main.js
+++ js/main.js
@@ -9,37 +9,45 @@ var unregister = function() {
 }
 
 //Initialize function
-var init = function () {
-    // register once
-    if ( backEventListener !== null ) {
-        return;
-    }
-    
-    // TODO:: Do your initialization job
-    console.log("init() called");
-    
-    var backEvent = function(e) {
-        if ( e.keyName == "back" ) {
-            try {
-                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
-                    // if first page, terminate app
-                    unregister();
-                } else {
-                    $.mobile.urlHistory.activeIndex -= 1;
-                    $.mobile.urlHistory.clearForward();
-                    window.history.back();
-                }
-            } catch( ex ) {
-                unregister();
-            }
-        }
-    }
-    
-    // add eventListener for tizenhwkey (Back Button)
-    document.addEventListener( 'tizenhwkey', backEvent );
-    backEventListener = backEvent;
-	document.addEventListener( 'keydown', setFocusElement );
-};
+// var init = function () {
+//     // register once
+//     if ( backEventListener !== null ) {
+//         return;
+//     }
+//
+//     // TODO:: Do your initialization job
+//     console.log("init() called");
+//
+//     var backEvent = function(e) {
+//         if ( e.keyName == "back" ) {
+//             try {
+//                 if ( $.mobile.urlHistory.activeIndex <= 0 ) {
+//                     // if first page, terminate app
+//                     unregister();
+//                 } else {
+//                     $.mobile.urlHistory.activeIndex -= 1;
+//                     $.mobile.urlHistory.clearForward();
+//                     window.history.back();
+//                 }
+//             } catch( ex ) {
+//                 unregister();
+//             }
+//         }
+//     }
+//
+//     // add eventListener for tizenhwkey (Back Button)
+//     document.addEventListener( 'tizenhwkey', backEvent );
+//     backEventListener = backEvent;
+// 	// document.addEventListener( 'keydown', setFocusElement );
+// };
+//
+// $(document).bind( 'pageinit', init );
+// $(document).unload( unregister );
 
-$(document).bind( 'pageinit', init );
-$(document).unload( unregister );
+var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+// console.log($('body'));
+// setTimeout(function () {
+//
+// $('body').html($('<div>').text(raw ? parseInt(raw[2], 10) : false));
+// }, 2000);
---
+++ js/services.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vadym Yatsyuk on 19/03/16
+ */
+
+angular.module('services', [])
+  .factory('APIService', function() {
+    return {
+      
+    }
+  });
